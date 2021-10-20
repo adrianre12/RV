db3a73573eb839a2a41814327c1c1681ce1e96a7
adrianre12 <adrianrevill@hotmail.com>
2021-10-20 14:38:10 +0100

    Added ShowAiPath

---
+++ Assets/_Project/Scripts/ShowAiPath.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShowAiPath : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    private float elapsed;
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        if (_navMeshAgent == null)
+        {
+            Debug.Log("No NavMeshAgent found");
+            return;
+        }
+        StartCoroutine(Draw());
+    }
+
+    private IEnumerator Draw()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f); //draw every 1s;
+            if (_navMeshAgent.hasPath)
+            {
+                Vector3[] corners = _navMeshAgent.path.corners;
+                for (int i = 0; i < corners.Length - 1; i++)
+                    Debug.DrawLine(corners[i], corners[i + 1], Color.red, 1f, false);
+            }
+        }
+    }
+}
